982850dadec2eb7e7469de40f6e1bfe527c373c1
chaima <chaimabouketta30@gmail.com>
2025-12-27 14:49:00 +0100

    Update User and Product model classes

--- MyGitApp/src/main/java/com/l3/gl/model/Product.java
+++ MyGitApp/src/main/java/com/l3/gl/model/Product.java
@@ -3,8 +3,20 @@ public class Product {
 private String name; 
 private double price; 
 public Product(String name, double price) { 
-this.name = name; 
-this.price = price; 
+this.setName(name); 
+this.setPrice(price); 
 } 
 // Getters and setters 
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
 }
